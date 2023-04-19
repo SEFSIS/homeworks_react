a4009359d7d6f2e8db0d27f64c7260f23a308b44
SEFSIS <sofiagryn@icloud.com>
2023-04-19 19:54:51 +0300

    homework_2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Posts} from "./components/Posts/Posts";
+import {Launches} from "./components/Launches/Launches";
+import {Users} from "./components/Users/Users";
+import {UserPosts} from "./components/UserPosts/UserPosts";
 
 const App = () => {
+    const [userId, setUserId] = useState(null);
     return (
         <div>
-          App
+            {/*<Posts/>*/}
+            {/*<Launches/>*/}
+            <Users setUserId={setUserId}/>
+            {userId && <UserPosts userId={userId} />}
         </div>
     );
 };
---
+++ src/components/Post/Post.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+// const Post = ({post,setPost}) => {
+const Post = ({post}) => {
+    const [show, setShow] = useState(false);
+    const {id, title} = post;
+
+    return (
+        <div>
+            <div>id:{id}</div>
+            <div>title:{title}</div>
+            <button onClick={() => setShow(prev => !prev)}>details</button>
+            <hr/>
+            {
+                show &&
+                <div>
+                <div>id:{post.id}</div>
+                <div>title:{post.title}</div>
+                <div>body:{post.body}</div>
+                <div>userId:{post.userId}</div>
+            </div>
+            }
+        </div>
+    );
+};
+export {Post};
+
