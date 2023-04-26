8baf4fb403f82a9ee3ef243c7e52ca487b9d1ced
SEFSIS <sofiagryn@icloud.com>
2023-04-26 19:21:50 +0300

    homework_5

--- src/components/Cat/Cat.js
+++ src/components/Cat/Cat.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import css from './Cat.module.css'
 
 const Cat = ({cat,dispatch}) => {
     const {id,name} = cat;
     return (
         <div>
 
-            <div>{id} {name}</div>
+            <div className={css.cat}>{id} {name}</div>
             <button onClick={()=>dispatch({type:'DELETE_CAT',payload:id})}>DELETE</button>
         </div>
     );
--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import css from './Dog.module.css'
 
 const Dog = ({dog,dispatch}) => {
     const {id,name} = dog;
     return (
-        <div>
-            <div>{id} {name}</div>
+        <div className={css.dog}>
+            <div >{id} {name}</div>
             <button onClick={()=>dispatch({type:'DELETE_DOG',payload:id})}>DELETE</button>
         </div>
     );
